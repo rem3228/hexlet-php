033f039e997efb5708d4750cd73aa553bb374f2e
Remag Seven <remag77777777@gmail.com>
2024-08-29 17:49:03 +0300

    fix bugs with phpcs

--- src/Runner.php
+++ src/Runner.php
@@ -4,9 +4,8 @@ namespace Hexlet\Php\Runner;
 
 function run()
 {
-	$collection = collect(['taylor', 'abigail', 'ivan'])->map(function($name)
-	{
-		return strtoupper($name);
-	});
-	return $collection;
+    $collection = collect(['taylor', 'abigail', 'ivan'])->map(function ($name) {
+        return strtoupper($name);
+    });
+    return $collection;
 }
